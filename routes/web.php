dfd47708b6fc8501fb453787c7c0af634c1f5e50
miguel.castaneda <miguel.castaneda@parservicios.com>
2017-08-10 14:18:29 -0500

    modificaciones

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,32 @@
 Auth::routes();
 Route::get('/logout', 'Auth\LoginController@logout');
 Route::get('/', 'HomeController@index');
+
+Route::get('/callback', function(){
+
+    if (! empty(request()->code)){
+        $code = urldecode(request()->code);
+
+        $http = new GuzzleHttp\Client;
+        $response = $http->post('http://phoneup.api.dev/oauth/token', [
+            'form_params' => [
+                'grant_type' => 'authorization_code',
+                'client_id' => '3',
+                'client_secret' => '0Uh2jbi5CWlAY68XcX9N40dWQPCN0caX4qWD8Fju',
+                'redirect_uri' => 'http://phoneup.client.dev/callback',
+                'code' => $code,
+            ],
+        ]);
+
+        dd(json_decode((string) $response->getBody(), true));
+    }
+
+    //
+    dd('hola');
+    //
+    //return json_decode((string) $response->getBody(), true);
+});
+
+Route::get('viewToken', function(){
+    dd(request()->all());
+});
